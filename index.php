e52f0198b4acbf9b3f26db02629ae8425c40021b
worljey <40305343+worljey@users.noreply.github.com>
2024-03-30 18:29:53 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div class="login-form">
+		<h2>Login Form</h2>
+		<form>
+			<label for="username">Username:</label><br>
+			<input type="text" id="username" name="username"><br>
+			<label for="password">Password:</label><br>
+			<input type="password" id="password" name="password"><br>
+			<input type="submit" value="Submit">
+		</form>
+	</div>
+</body>
+</html>
